1abdb1c9c8524f225cf41e58c9313a88bda9a529
Bhagyaraju <bhagyaraju@bhagyaraju.local>
2024-03-13 09:59:59 +0530

    Initial Commit

---
+++ MapKitPractice/MapKitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitPracticeApp.swift
+//  MapKitPractice
+//
+//  Created by Bhagyaraju on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
